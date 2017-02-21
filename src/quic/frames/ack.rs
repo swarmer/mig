6e812060a8814dcd4f48a62e547bf52cee03a785
Anton Barkovsky <anton@swarmer.me>
2017-02-22 00:20:09 +0300

    Add decoding for the ack frame

--- src/quic/frames/ack.rs
+++ src/quic/frames/ack.rs
@@ -1,18 +1,19 @@
 use std::io;
 
-use byteorder::{BigEndian, WriteBytesExt};
+use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
 use cast;
 
 use quic::errors::Result;
-use quic::utils;
+use quic::utils::{map_unexpected_eof, truncate_u64};
 
 
 pub const FRAME_FLAG_ACK: u8 = 0b01000000;
+pub const FRAME_MASK_ACK: u8 = 0b11000000;
 
 #[derive(Clone, Copy, Debug, Default, PartialEq)]
 pub struct ExtraAckBlock {
     pub gap: u8,
-    pub block_count: u64,
+    pub block_length: u64,
 }
 
 #[derive(Clone, Copy, Debug, Default, PartialEq)]
@@ -34,7 +35,7 @@ pub struct AckFrame {
     pub ack_delay: u16,
 
     // ack block section
-    pub first_ack_block_count: u64,
+    pub first_ack_block_length: u64,
     pub extra_ack_blocks: Vec<ExtraAckBlock>,
 
     // timestamp section
@@ -52,11 +53,11 @@ impl AckFrame {
         }
 
         // TODO: calculate this more intelligently
-        let largest_ack_length = 6;
+        let largest_ack_size = 6;
         frame_type |= 0b00001100;
 
         // TODO: calculate this more intelligently
-        let ack_block_length = 6;
+        let ack_block_size = 6;
         frame_type |= 0b00000011;
 
         write.write_u8(frame_type)?;
@@ -76,16 +77,16 @@ impl AckFrame {
             .expect("Too many timestamp blocks, count has to fit in 8 bits")
         )?;
         write.write_uint::<BigEndian>(
-            utils::truncate_u64(self.largest_acknowledged, largest_ack_length),
-            largest_ack_length,
+            truncate_u64(self.largest_acknowledged, largest_ack_size),
+            largest_ack_size,
         )?;
         write.write_u16::<BigEndian>(self.ack_delay)?;
 
         // ack block section
-        write.write_uint::<BigEndian>(self.first_ack_block_count, ack_block_length)?;
+        write.write_uint::<BigEndian>(self.first_ack_block_length, ack_block_size)?;
         for ack_block in &self.extra_ack_blocks[..] {
             write.write_u8(ack_block.gap)?;
-            write.write_uint::<BigEndian>(ack_block.block_count, ack_block_length)?;
+            write.write_uint::<BigEndian>(ack_block.block_length, ack_block_size)?;
         }
 
         // timestamp section
@@ -102,5 +103,92 @@ impl AckFrame {
         }
         
         Ok(())
+    }
+
+    pub fn decode(read: &mut io::Read) -> Result<AckFrame> {
+        // extract type octet data
+        let frame_type = read.read_u8().map_err(map_unexpected_eof)?;
+        if (frame_type & FRAME_MASK_ACK) != FRAME_FLAG_ACK {
+            panic!("Incorrect frame's decode called!")
+        }
+
+        let has_extra_ack_blocks = (frame_type & 0b00100000) != 0;
+
+        let largest_ack_size = match (frame_type & 0b00001100) >> 2 {
+            0b00 => 1,
+            0b01 => 2,
+            0b10 => 4,
+            0b11 => 6,
+            _ => unreachable!(),
+        };
+
+        let ack_block_size = match frame_type & 0b00000011 {
+            0b00 => 1,
+            0b01 => 2,
+            0b10 => 4,
+            0b11 => 6,
+            _ => unreachable!(),
+        };
+
+        // other fields
+        let extra_ack_block_count = if has_extra_ack_blocks {
+            read.read_u8().map_err(map_unexpected_eof)? as usize
+        } else {
+            0
+        };
+
+        let timestamp_count = read.read_u8().map_err(map_unexpected_eof)? as usize;
+
+        let largest_acknowledged = 
+            read.read_uint::<BigEndian>(largest_ack_size)
+            .map_err(map_unexpected_eof)?;
+        
+        let ack_delay = 
+            read.read_u16::<BigEndian>()
+            .map_err(map_unexpected_eof)?;
+        
+        // ack block section
+        let first_ack_block_length = 
+            read.read_uint::<BigEndian>(ack_block_size)
+            .map_err(map_unexpected_eof)?;
+        
+        let mut extra_ack_blocks = Vec::new();
+        for _ in 0..extra_ack_block_count {
+            let gap = read.read_u8().map_err(map_unexpected_eof)?;
+            let block_length = 
+                read.read_uint::<BigEndian>(ack_block_size)
+                .map_err(map_unexpected_eof)?;
+
+            extra_ack_blocks.push(ExtraAckBlock { gap: gap, block_length: block_length });
+        }
+
+        // timestamp section
+        let first_timestamp = if timestamp_count > 0 {
+            let delta_la = read.read_u8().map_err(map_unexpected_eof)?;
+            let delta_timestamp = read.read_u32::<BigEndian>().map_err(map_unexpected_eof)?;
+            Some(FirstAckTimestamp { delta_la: delta_la, delta_timestamp: delta_timestamp })
+        } else {
+            None
+        };
+
+        let mut extra_timestamps = Vec::new();
+        if timestamp_count > 1 {
+            for _ in 0..(timestamp_count - 1) {
+                let delta_la = read.read_u8().map_err(map_unexpected_eof)?;
+                let delta_timestamp = read.read_u16::<BigEndian>().map_err(map_unexpected_eof)?;
+                extra_timestamps.push(ExtraAckTimestamp { delta_la: delta_la, delta_timestamp: delta_timestamp })
+            }
+        }
+
+        Ok(AckFrame {
+            largest_acknowledged: largest_acknowledged,
+            ack_delay: ack_delay,
+
+            first_ack_block_length: first_ack_block_length,
+            extra_ack_blocks: extra_ack_blocks,
+
+            first_timestamp: first_timestamp,
+            extra_timestamps: extra_timestamps,
+        })
     }
 }
--- src/tests/quic/frames/ack.rs
+++ src/tests/quic/frames/ack.rs
@@ -1,5 +1,6 @@
 use std::io;
 
+use quic::errors::Error;
 use quic::frames::ack;
 
 
@@ -9,7 +10,7 @@ fn test_encoding() {
         largest_acknowledged: 42,
         ack_delay: 32,
 
-        first_ack_block_count: 64,
+        first_ack_block_length: 64,
         extra_ack_blocks: Vec::new(),
 
         first_timestamp: None,
@@ -33,10 +34,10 @@ fn test_encoding() {
         largest_acknowledged: 42,
         ack_delay: 32,
 
-        first_ack_block_count: 64,
+        first_ack_block_length: 64,
         extra_ack_blocks: vec![
-            ack::ExtraAckBlock { gap: 31, block_count: 0xDEADCAFE },
-            ack::ExtraAckBlock { gap: 33, block_count: 0xAABBCCDDEEFF },
+            ack::ExtraAckBlock { gap: 31, block_length: 0xDEADCAFE },
+            ack::ExtraAckBlock { gap: 33, block_length: 0xAABBCCDDEEFF },
         ],
 
         first_timestamp: Some(ack::FirstAckTimestamp { delta_la: 1, delta_timestamp: 0xAABBCCDD }),
@@ -67,4 +68,97 @@ fn test_encoding() {
             0xBB, 0xCC,
         ]
     );
+}
+
+#[test]
+fn test_decoding() {
+    let mut read = io::Cursor::new(
+        vec![
+            0x4F,
+            0x00,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x2A,
+            0x00, 0x20,
+
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x40,
+        ]
+    );
+    let frame = ack::AckFrame::decode(&mut read).unwrap();
+    assert_eq!(
+        frame,
+        ack::AckFrame {
+            largest_acknowledged: 42,
+            ack_delay: 32,
+
+            first_ack_block_length: 64,
+            extra_ack_blocks: Vec::new(),
+
+            first_timestamp: None,
+            extra_timestamps: Vec::new(),
+        }
+    );
+
+    let mut read = io::Cursor::new(
+        vec![
+            0x6F,
+            0x02,
+            0x02,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x2A,
+            0x00, 0x20,
+
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x40,
+            0x1F,
+            0x00, 0x00, 0xDE, 0xAD, 0xCA, 0xFE,
+            0x21,
+            0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF,
+
+            0x01,
+            0xAA, 0xBB, 0xCC, 0xDD,
+            0x02,
+            0xBB, 0xCC,
+        ]
+    );
+    let frame = ack::AckFrame::decode(&mut read).unwrap();
+    assert_eq!(
+        frame,
+        ack::AckFrame {
+            largest_acknowledged: 42,
+            ack_delay: 32,
+
+            first_ack_block_length: 64,
+            extra_ack_blocks: vec![
+                ack::ExtraAckBlock { gap: 31, block_length: 0xDEADCAFE },
+                ack::ExtraAckBlock { gap: 33, block_length: 0xAABBCCDDEEFF },
+            ],
+
+            first_timestamp: Some(ack::FirstAckTimestamp { delta_la: 1, delta_timestamp: 0xAABBCCDD }),
+            extra_timestamps: vec![
+                ack::ExtraAckTimestamp { delta_la: 2, delta_timestamp: 0xBBCC },
+            ],
+        }
+    );
+
+    let mut read = io::Cursor::new(
+        vec![
+            0x6F,
+            0x02,
+            0x02,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x2A,
+            0x00, 0x20,
+
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x40,
+            0x1F,
+            0x00, 0x00, 0xDE, 0xAD, 0xCA, 0xFE,
+            0x21,
+            0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF,
+
+            0x01,
+            0xAA, 0xBB, 0xCC, 0xDD,
+            0x02,
+            0xBB,
+        ]
+    );
+    match ack::AckFrame::decode(&mut read) {
+        Err(Error::Decoding(_)) => {},
+        _ => assert!(false, "Error expected"),
+    };
 }
