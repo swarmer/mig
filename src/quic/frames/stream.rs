df2306f9c32b1d8f00aba659917e08bdaa80caa2
Anton Barkovsky <anton@swarmer.me>
2017-02-21 20:29:26 +0300

    Add decoding for the stream frame

--- src/quic/frames/stream.rs
+++ src/quic/frames/stream.rs
@@ -1,9 +1,10 @@
 use std::io;
 
-use byteorder::{BigEndian, WriteBytesExt};
+use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
 use cast;
 
 use quic::errors::Result;
+use quic::utils::map_unexpected_eof;
 
 
 pub const FRAME_FLAG_STREAM: u8 = 0b10000000;
@@ -51,5 +52,63 @@ impl StreamFrame {
         write.write_all(&self.stream_data[..])?;
 
         Ok(())
+    }
+
+    pub fn decode(read: &mut io::Read) -> Result<StreamFrame> {
+        // extract type octet data
+        let frame_type = read.read_u8().map_err(map_unexpected_eof)?;
+        if (frame_type & FRAME_FLAG_STREAM) == 0 {
+            panic!("Incorrect frame's decode called!")
+        }
+
+        let fin = (frame_type & 0b01000000) != 0;
+
+        let last_frame = (frame_type & 0b00100000) == 0;
+
+        let offset_length = match (frame_type & 0b00011100) >> 2 {
+            0b000 => 0,
+            bit_value => bit_value + 1,
+        } as usize;
+
+        let stream_id_length = ((frame_type & 0b00000011) + 1) as usize;
+
+        // other fields
+        let data_length = if !last_frame {
+            Some(read.read_u16::<BigEndian>().map_err(map_unexpected_eof)? as usize)
+        } else {
+            None
+        };
+
+        let stream_id = 
+            read.read_uint::<BigEndian>(stream_id_length)
+            .map_err(map_unexpected_eof)?
+            as u32;
+        
+        let offset = if offset_length != 0 {
+            read.read_uint::<BigEndian>(offset_length)
+            .map_err(map_unexpected_eof)?
+        } else {
+            0
+        };
+
+        let stream_data = match data_length {
+            Some(data_length) => {
+                let mut buffer = vec![0; data_length];
+                read.read_exact(&mut buffer).map_err(map_unexpected_eof)?;
+                buffer
+            },
+            None => {
+                let mut buffer = Vec::new();
+                read.read_to_end(&mut buffer).map_err(map_unexpected_eof)?;
+                buffer
+            },
+        };
+
+        Ok(StreamFrame {
+            stream_id: stream_id,
+            offset: offset,
+            stream_data: stream_data,
+            fin: fin,
+        })
     }
 }
--- src/tests/quic/frames/stream.rs
+++ src/tests/quic/frames/stream.rs
@@ -1,5 +1,6 @@
 use std::io;
 
+use quic::errors::Error;
 use quic::frames::stream;
 
 
@@ -41,4 +42,60 @@ fn test_encoding() {
             0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x32,
         ]
     );
+}
+
+#[test]
+fn test_decoding() {
+    let mut read = io::Cursor::new(
+        vec![
+            0xBF,
+            0x00, 0x05,
+            0x00, 0x00, 0x00, 0x2A,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x20,
+            0x68, 0x65, 0x6C, 0x6C, 0x6F,
+        ]
+    );
+    let frame = stream::StreamFrame::decode(&mut read).unwrap();
+    assert_eq!(
+        frame,
+        stream::StreamFrame {
+            stream_id: 42,
+            offset: 32,
+            stream_data: vec![0x68, 0x65, 0x6C, 0x6C, 0x6F],
+            fin: false,
+        }
+    );
+
+    let mut read = io::Cursor::new(
+        vec![
+            0xDF,
+            0xDE, 0xAD, 0xCA, 0xFE,
+            0x10, 0x11, 0x12, 0x13, 0xDE, 0xAD, 0xCA, 0xFE,
+            0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x32,
+        ]
+    );
+    let frame = stream::StreamFrame::decode(&mut read).unwrap();
+    assert_eq!(
+        frame,
+        stream::StreamFrame {
+            stream_id: 0xDEADCAFE,
+            offset: 0x10111213DEADCAFE,
+            stream_data: vec![0x68, 0x65, 0x6C, 0x6C, 0x6F, 0x32],
+            fin: true,
+        }
+    );
+
+    let mut read = io::Cursor::new(
+        vec![
+            0xBF,
+            0x00, 0x05,
+            0x00, 0x00, 0x00, 0x2A,
+            0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x20,
+            0x68, 0x65, 0x6C, 0x6C,
+        ]
+    );
+    match stream::StreamFrame::decode(&mut read) {
+        Err(Error::Decoding(_)) => {},
+        _ => assert!(false, "Error expected"),
+    };
 }
