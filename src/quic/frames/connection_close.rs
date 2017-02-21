42579eb7da8e7a99bd486862d9cdbda8c65f3f09
Anton Barkovsky <anton@swarmer.me>
2017-02-21 16:38:35 +0300

    Add decoding for the connection_close frame

--- src/quic/frames/connection_close.rs
+++ src/quic/frames/connection_close.rs
@@ -1,14 +1,15 @@
 use std::io;
 
-use byteorder::{BigEndian, WriteBytesExt};
+use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
 use cast;
 
 use quic::errors::Result;
+use quic::utils::map_unexpected_eof;
 
 
 pub const FRAME_CONNECTION_CLOSE: u8 = 0x02;
 
-#[derive(Clone, Debug, Default)]
+#[derive(Clone, Debug, Default, PartialEq)]
 pub struct ConnectionCloseFrame {
     pub error_code: u32,
     pub reason_phrase: Option<String>,
@@ -34,5 +35,33 @@ impl ConnectionCloseFrame {
         }
 
         Ok(())
+    }
+
+    pub fn decode(read: &mut io::Read) -> Result<ConnectionCloseFrame> {
+        if read.read_u8().map_err(map_unexpected_eof)? != FRAME_CONNECTION_CLOSE {
+            panic!("Incorrect frame's decode called!")
+        }
+
+        let error_code = 
+            read.read_u32::<BigEndian>()
+            .map_err(map_unexpected_eof)?;
+        let reason_phrase_length = 
+            read.read_u16::<BigEndian>()
+            .map_err(map_unexpected_eof)?
+            as usize;
+        
+        let reason_phrase = if reason_phrase_length != 0 {
+            let mut buf = vec![0; reason_phrase_length];
+            read.read_exact(&mut buf).map_err(map_unexpected_eof)?;
+            let reason_phrase = String::from_utf8_lossy(&buf).to_string();
+            Some(reason_phrase)
+        } else {
+            None
+        };
+
+        Ok(ConnectionCloseFrame {
+            error_code: error_code,
+            reason_phrase: reason_phrase,
+        })
     }
 }
--- src/tests/quic/frames/connection_close.rs
+++ src/tests/quic/frames/connection_close.rs
@@ -1,5 +1,6 @@
 use std::io;
 
+use quic::errors::Error;
 use quic::frames::connection_close;
 
 
@@ -35,4 +36,53 @@ fn test_encoding() {
             0x68, 0x65, 0x6C, 0x6C, 0x6F,
         ]
     );
+}
+
+#[test]
+fn test_decoding() {
+    let mut read = io::Cursor::new(
+        vec![
+            0x02,
+            0x00, 0x00, 0x00, 0x2A,
+            0x00, 0x00,
+        ]
+    );
+    let frame = connection_close::ConnectionCloseFrame::decode(&mut read).unwrap();
+    assert_eq!(
+        frame,
+        connection_close::ConnectionCloseFrame {
+            error_code: 42,
+            reason_phrase: None,
+        }
+    );
+
+    let mut read = io::Cursor::new(
+        vec![
+            0x02,
+            0x00, 0x00, 0x00, 0x2A,
+            0x00, 0x05,
+            0x68, 0x65, 0x6C, 0x6C, 0x6F,
+        ]
+    );
+    let frame = connection_close::ConnectionCloseFrame::decode(&mut read).unwrap();
+    assert_eq!(
+        frame,
+        connection_close::ConnectionCloseFrame {
+            error_code: 42,
+            reason_phrase: Some("hello".to_string()),
+        }
+    );
+
+    let mut read = io::Cursor::new(
+        vec![
+            0x02,
+            0x00, 0x00, 0x00, 0x2A,
+            0x00, 0x05,
+            0x68, 0x65, 0x6C, 0x6C,
+        ]
+    );
+    match connection_close::ConnectionCloseFrame::decode(&mut read) {
+        Err(Error::Decoding(_)) => {},
+        _ => assert!(false, "Error expected"),
+    };
 }
