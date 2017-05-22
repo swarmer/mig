13e902b0f4d8844d37d5b6b2cbb3b9fccdce2361
Vitaly Shukela <vi0oss@gmail.com>
2017-05-22 23:26:56 +0300

    Add initial hacky example of TUN server/client (#3)
    * Add incomplete hacky example of TUN server/client
    
    * Make it a bit less broken.
    
    Now the ping (but not ping replies) get though.
    
    * Improve the tun example.
    
    * Use separate streams for send and recv directions
    
    * Re-add the "negotiation" and implement loop restarter.

---
+++ examples/tun.rs
@@ -0,0 +1,142 @@
+#[macro_use]
+extern crate log;
+extern crate env_logger;
+extern crate tun;
+
+extern crate mig;
+
+use std::str;
+use std::io::{Write, Read};
+
+use mig::quic::threaded::{QuicConnection,QuicListener};
+
+use tun::{Device as TunDevice};
+
+use std::sync::Arc;
+use std::os::unix::io::{FromRawFd,AsRawFd};
+
+
+#[derive(Copy,Clone)]
+enum Mode { Listen, Connect, }
+
+fn get_connection(mode: Mode, address: &str) -> QuicConnection {
+    match mode {
+        Mode::Connect => {
+            info!("Establishing connection...");
+            let connection = match QuicConnection::new(address) {
+                Ok(connection) => {
+                    connection
+                },
+                Err(e) => {
+                    error!("Cannot create a connection: {}", e);
+                    ::std::process::exit(1)
+                },
+            };
+        
+            info!("Running client connected to {}", address);
+            connection
+        }
+        Mode::Listen => {
+            let listener = match QuicListener::bind(&*address) {
+                Ok(listener) => {
+                    listener
+                },
+                Err(e) => {
+                    error!("Cannot bind to the address: {}", e);
+                    panic!();
+                },
+            };
+            info!("Server on {}: listening for connections...", address);
+            let connection = match listener.accept() {
+                Ok(connection) => {
+                    connection
+                },
+                Err(e) => {
+                    error!("Cannot accept a connection: {}", e);
+                    panic!();
+                },
+            };
+            info!("Got a connection");
+            connection
+        }
+    }
+}
+
+fn main() {
+    env_logger::init().unwrap();
+
+    let args = ::std::env::args;
+    if args().len() != 4 || args().nth(1) == Some("--help".to_string()) {
+        println!("Usage: migtun <ifname> {{listen|connect}} <serverip>:<port>");
+        return;
+    }
+
+    let tunname = args().nth(1).unwrap();
+    let mode = match &*args().nth(2).unwrap() {
+        "listen" => Mode::Listen,
+        "connect" => Mode::Connect,
+        &_ => panic!("Mode should be only listen or connect")
+    };
+    let address = args().nth(3).unwrap();
+    info!("Creating tun device...");
+    
+    let mut dev = tun::create(tunname).unwrap();
+    
+    let connection = Arc::new(get_connection(mode, &*address));
+ 
+    info!("Set the IP address of {} youself", dev.name());
+    match mode {
+        Mode::Connect => println!("ifconfig {} 192.168.123.2", dev.name()),
+        Mode::Listen  => println!("ifconfig {} 192.168.123.1", dev.name()),
+    }
+    
+    
+    match mode { 
+        Mode::Connect => {
+            info!("Sending a byte");
+            let b=[0;1];
+            let mut stream = connection.get_stream(1);
+            stream.write_all(&b).unwrap();
+            
+        }
+        Mode::Listen  => {
+            info!("Receiving a byte");
+            let mut b=[0;1];
+            let mut stream = connection.get_stream(1);
+            stream.read_exact(&mut b).unwrap();
+        }
+    }
+    
+    
+    info!("Serving");
+    
+    
+    let connection_copy = connection.clone();
+    
+    // Hack:
+    let dev_copy = unsafe { ::std::fs::File::from_raw_fd(dev.as_raw_fd()) };
+  
+    let (stream_n_1, stream_n_2) = match mode {
+        Mode::Connect => (2,3),
+        Mode::Listen  => (3,2),
+    };
+    
+    ::std::thread::spawn(move || {
+        let mut stream2 = connection_copy.get_stream(stream_n_1);
+        let mut my_dev_copy = dev_copy;
+        
+        loop {
+            let _ = std::io::copy(&mut stream2, &mut my_dev_copy);
+            info!("Restarting loop 2");
+            ::std::thread::sleep(::std::time::Duration::new(0, 100_000_000));
+        }
+    });
+    
+    let mut stream = connection.get_stream(stream_n_2);
+    
+    loop {
+        let _ = std::io::copy(&mut dev, &mut stream);
+        info!("Restarting loop 1");
+        ::std::thread::sleep(::std::time::Duration::new(0, 100_000_000));
+    }
+}
